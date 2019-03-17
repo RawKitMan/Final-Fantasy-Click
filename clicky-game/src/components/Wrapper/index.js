7771e82e5b4032fbd7f28dc22c84ca71a8a255ae
RawKitMan <cjvaughn@rocketmail.com>
2019-03-16 22:44:45 -0700

    Working on Navbar coloring

--- clicky-game/src/components/Wrapper/index.js
+++ clicky-game/src/components/Wrapper/index.js
@@ -67,7 +67,7 @@ class Clicky extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar text-white text-center border-bottom border-dark wide-border fixed-top coloring">
+        <nav className="navbar coloring text-white text-center border-bottom border-dark wide-border fixed-top">
           <div className="container-fluid">
             <div className="row w-100">
               <div className="col-md-3 text-sizing">Click That Image!!</div>
